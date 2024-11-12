0e9227ef972244eab71ecee994e950f159f1f62b
ShashiMalik <147818128+ShashiMalik@users.noreply.github.com>
2024-11-12 17:26:05 +0530

    Data Transformation Added

--- src/mlproject_template/pipepline/stage_03_data_transformation.py
+++ src/mlproject_template/pipepline/stage_03_data_transformation.py
@@ -9,19 +9,22 @@ class DataTransformationTrainingPipeline:
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation =  DataTransformation(config = data_transformation_config)
-        data_transformation.train_test_spliting()
+        try:
+            with open(Path("artifacts/data_validation/status.txt"),'r') as f:
+                status = f.read().split(" ")[-1]
 
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<< ")
-        obj = DataTransformationTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<< ")
-    except Exception as e:
-        raise print(e)
+                if status == "True":
+                    config = ConfigurationManager()
+                    data_transformation_config = config.get_data_transformation_config()
+                    data_transformation =  DataTransformation(config = data_transformation_config)
+                    data_transformation.train_test_spliting()
+
+                else:
+                    raise Exception("Your Data Schema is not valid")
+        
+        except Exception as e:
+            print(e)
+
 
                     
 
